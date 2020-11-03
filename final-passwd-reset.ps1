794b32d764a0ae797ac030a90c0641c875cfcdb5
vijishankar <67778907+vijishankar@users.noreply.github.com>
2020-11-03 11:03:10 +0530

    Update final-passwd-reset.ps1

--- final-passwd-reset.ps1
+++ final-passwd-reset.ps1
@@ -7,6 +7,29 @@ param(
 	$SecretName
 )
 
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
 
  $sqlserver = Get-AzureRmSqlServer -ResourceGroupName $ResourceGroupName
  $keyVault=Get-AzureRMKeyVault -VaultName $keyVaultName -ErrorVariable notPresent -ErrorAction SilentlyContinue
